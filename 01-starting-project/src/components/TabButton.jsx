6f90d97cb82f61b890c5b859064188df19e72bd0
Miguel Vazquez <mvazque93@gmail.com>
2024-07-17 13:03:20 -0700

    feat(Section-4): Updated code to make components more flexible.

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,9 +1,9 @@
-export default function TabButton({ children, onSelect, isSelected }) {
-    
-
+export default function TabButton({ children, onSelect, isSelected, ...props }) {
     return (
         <li>
-            <button className={isSelected ? 'active': ''} onClick={onSelect}>{children}</button>
+            <button className={isSelected ? 'active': ''} {...props}>
+                {children}
+            </button>
         </li>
     );
 }
