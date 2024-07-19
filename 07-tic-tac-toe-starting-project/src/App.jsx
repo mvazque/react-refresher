53a210fe0c215767c295b76a7ea62fc9e96a34e4
Miguel Vazquez <mvazque93@gmail.com>
2024-07-18 20:13:56 -0700

    feat(Sect-4): Added winning combinations check to tic tac toe game.

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,9 +1,83 @@
+import { useState } from "react"
+import GameBoard from "./components/GameBoard"
+import Player from "./components/Player"
+import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+  if(gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
+
+const initialGameBoard = [
+  [ null, null, null ],
+  [ null, null, null ],
+  [ null, null, null ]
+]
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+  // const [activePlayer, setActivePlayer] = useState('X');
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+    for(const turn of gameTurns){
+        const { square, player } = turn;
+        const { row, col } = square;
+
+        gameBoard[row][col] = player;
+    }
+
+  let winner;
+
+  if(gameTurns.length > 4) {
+    for(const combination of WINNING_COMBINATIONS){
+      const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+      const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+      const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+  
+      if( firstSquareSymbol && 
+          firstSquareSymbol === secondSquareSymbol && 
+          firstSquareSymbol === thirdSquareSymbol
+        ) {
+          winner = firstSquareSymbol;
+          console.log("Winner");
+      }
+    }
+  }
   
 
+  function handleSelectSquare(rowIndex, colIndex) {
+    // setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns(prevTurns => {
+      // let currentPlayer = 'X';
+      // if(prevTurns.length > 0 && prevTurns[0].player === 'X') {
+      //   currentPlayer = 'O';
+      // }
+      const currentPlayer = deriveActivePlayer(prevTurns);
+      const updatedTurns = [{ square: {row: rowIndex, col: colIndex}, player: currentPlayer } ,...prevTurns];
+      return updatedTurns;
+    });
+  }
+
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}></Player>
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}></Player>
+        </ol>
+        {winner && <p>You won, {winner}!</p>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
+      </div>
+      <Log turns={gameTurns}></Log>
+    </main>
+    
   )
 }
 
