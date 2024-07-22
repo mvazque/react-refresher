ee2e02c49d02d58caf92a9acc8f8653f60f7b8fd
Miguel Vazquez <mvazque93@gmail.com>
2024-07-22 14:35:39 -0700

    feat(Sect-5): Completed programming exercise.

--- 05-investment-project/src/App.jsx
+++ 05-investment-project/src/App.jsx
@@ -1,6 +1,34 @@
+import Header from "./components/Header"
+import Results from "./components/Results";
+import UserInput from "./components/UserInput"
+import { useState } from "react";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue){
+    setUserInput((prevState) => {
+      return {
+          ...prevState,
+          [inputIdentifier]: +newValue
+      };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+      <Header />
+      <UserInput onChangeInput={handleChange} userInput={userInput}/>
+      {!inputIsValid && <p className="center">Please enter a duration greater than 0.</p>}
+      {inputIsValid && <Results input={userInput}></Results> }
+    </>
   )
 }
 
