6c84258072f6359f375e69253717683cf9e03d6b
Miguel Vazquez <mvazque93@gmail.com>
2024-07-29 10:22:51 -0700

    Created reusable modal and added styling.

--- 09-project-managment/src/App.jsx
+++ 09-project-managment/src/App.jsx
@@ -15,6 +15,15 @@ function App() {
     });
   }
 
+  function handleCancelAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined
+      };
+    });
+  }
+
   function handleAddProject(projectData) {
     setProjectsState((prevState) => {
       const projectId = Math.random();
@@ -31,10 +40,9 @@ function App() {
     });
   }
 
-  console.log(projectsState);
   let content;
   if(projectsState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject}/>;
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>;
   } else if(projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
