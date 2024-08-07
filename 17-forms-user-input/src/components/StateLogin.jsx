bb6ded83930818719423367780dff33678097e15
Miguel Vazquez <mvazque93@gmail.com>
2024-08-06 17:37:59 -0700

    Feat(Sect-17): Playing around with validation techniques.

--- 17-forms-user-input/src/components/StateLogin.jsx
+++ 17-forms-user-input/src/components/StateLogin.jsx
@@ -6,6 +6,7 @@ export default function StateLogin() {
     password: ''
   });
 
+  const emailIsInvalid = enteredValues.email !== '' && !enteredValues.email.includes('@');
 
   function handleInputChange(identifier, value) {
     setEnteredValues(prevValues => ({
@@ -17,7 +18,10 @@ export default function StateLogin() {
   function handleSubmit(event) {
     event.preventDefault();
     console.log(enteredValues);
-
+    setEnteredValues({
+      email: '',
+      password: ''
+    })
   }
 
   return (
@@ -34,6 +38,7 @@ export default function StateLogin() {
             onChange={(event) => handleInputChange('email', event.target.value)}
             value={enteredValues.email}
           />
+          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address.</p>}</div>
         </div>
 
         <div className="control no-margin">
