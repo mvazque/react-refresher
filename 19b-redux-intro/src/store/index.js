1063c6e439ddddc9f2e3b48882d7afbee4ce5382
Miguel Vazquez <mvazque93@gmail.com>
2024-08-10 13:20:01 -0700

    feat(Sect-19b): Created initial store and reducer.

---
+++ 19b-redux-intro/src/store/index.js
@@ -0,0 +1,22 @@
+import { createStore } from 'redux';
+
+function counterReducer(state = { count: 0}, action) {
+    if(action.type === 'increment'){
+        return {
+            count: state.count + 1
+        }
+    }
+
+    if(action.type === 'decrement'){
+        return {
+            count: state.count - 1
+        }
+    }
+
+    return state;
+}
+
+
+const store = createStore(counterReducer);
+
+export default store;
