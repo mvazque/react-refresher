e111836071773cdb12755d3f95880e08cede147c
Miguel Vazquez <mvazque93@gmail.com>
2024-08-13 14:47:57 -0700

    feat(Sect-20): Fixes to bugs where backend had undefined cart values.

--- 20-redux-deep-dive/src/store/cart-actions.js
+++ 20-redux-deep-dive/src/store/cart-actions.js
@@ -15,10 +15,11 @@ export const fetchCartData = () => {
         };
 
         try {
-            console.log('Attempt fetch');
             const cartData = await fetchData();
-            console.log(cartData);
-            dispatch(cartActions.replaceCart(cartData));
+            dispatch(cartActions.replaceCart({
+                items: cartData.items || [],
+                totalQuantity: cartData.totalQuantity
+            }));
 
         } catch (error) {
             dispatch(uiActions.showNotification({
@@ -44,7 +45,10 @@ export const sendCartData = (cart) => {
             const response = await fetch('https://react-advanced-redux-d9c3b-default-rtdb.firebaseio.com/cart.json',
                 {
                     method: 'PUT',
-                    body: JSON.stringify(cart)
+                    body: JSON.stringify({
+                        items: cart.items,
+                        totalQuantity: cart.totalQuantity
+                    })
                 }
             );
     
