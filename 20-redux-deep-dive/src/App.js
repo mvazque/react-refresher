79d40aca82c4c75576d9eb20fa151d9887aaf7e1
Miguel Vazquez <mvazque93@gmail.com>
2024-08-13 14:08:36 -0700

    feat(Sect-20): Implemented a custom action within cart-slice to handle PUT requests to the backend.

--- 20-redux-deep-dive/src/App.js
+++ 20-redux-deep-dive/src/App.js
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { uiActions } from './store/ui-slice';
 import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
 
@@ -15,50 +16,11 @@ function App() {
   const notification = useSelector(state => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-
-      if(isInitial){
-        isInitial = false;
-        return;
-      }
-      
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!'
-      }));
-
-      const response = await fetch('https://react-advanced-redux-d9c3b-default-rtdb.firebaseio.com/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart)
-        }
-      );
-
-      if(!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title: 'Success!',
-        message: 'Sent cart data successfully.'
-      }));
-
-    
-
-      const responseData = await response.json();
+    if(isInitial){
+      isInitial = false;
+      return;
     }
-
-    sendCartData().catch(error => {
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error!',
-        message: 'Sending cart data failed.'
-      }));
-    });
-    
-    
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
