43c8b26483f5f5f4ad516fdf3cf93ccb153c3f16
Miguel Vazquez <mvazque93@gmail.com>
2024-08-13 13:12:10 -0700

    feat(Sect-20): Implemented notification component and added state to UI slice that will be in charge of it.

--- 20-redux-deep-dive/src/App.js
+++ 20-redux-deep-dive/src/App.js
@@ -1,27 +1,79 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
+import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
+
+let isInitial = true;
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+  const notification = useSelector(state => state.ui.notification);
 
   useEffect(() => {
-    fetch('https://react-advanced-redux-d9c3b-default-rtdb.firebaseio.com/cart.json',
-      {
-        method: 'PUT',
-        body: JSON.stringify(cart)
+    const sendCartData = async () => {
+
+      if(isInitial){
+        isInitial = false;
+        return;
       }
-    )
-  }, [cart]);
-  
+      
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!'
+      }));
+
+      const response = await fetch('https://react-advanced-redux-d9c3b-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart)
+        }
+      );
+
+      if(!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Sent cart data successfully.'
+      }));
+
+    
+
+      const responseData = await response.json();
+    }
+
+    sendCartData().catch(error => {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error!',
+        message: 'Sending cart data failed.'
+      }));
+    });
+    
+    
+  }, [cart, dispatch]);
+
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && <Notification 
+        status={notification.status} 
+        title={notification.title} 
+          message={notification.mesage}
+        />}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
+    
   );
 }
 
