5c66ee767b28b2485a0459b55a1fee644da154e9
Miguel Vazquez <mvazque93@gmail.com>
2024-08-14 12:45:08 -0700

    feat(Sect-20a): Changed some absolute paths to relative. Added a back button for product details.

--- 21a-intro-to-routing/src/pages/Products.js
+++ 21a-intro-to-routing/src/pages/Products.js
@@ -11,9 +11,9 @@ function ProductsPage() {
         <>
             <h1>The Products Page</h1>
             <ul>
-                {PRODUCTS.map((product) => <li key={product.id}>
-                        <Link to={`/products/${product.id}`}>{product.title}</Link>
-
+                {PRODUCTS.map((product) => 
+                    <li key={product.id}>
+                        <Link to={product.id}>{product.title}</Link>
                     </li>
                 )}
             </ul>
