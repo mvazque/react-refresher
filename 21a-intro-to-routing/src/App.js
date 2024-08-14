d1fb39578f52de1e5f5bd9ccf8655f4d9d7c7339
Miguel Vazquez <mvazque93@gmail.com>
2024-08-14 11:34:34 -0700

    feat(Sect-21a): Implemented error page to catch erros from root route.

--- 21a-intro-to-routing/src/App.js
+++ 21a-intro-to-routing/src/App.js
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
+import ErrorPage from './pages/Error';
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -15,10 +16,11 @@ const router = createBrowserRouter([
   { 
     path: '/', 
     element: <RootLayout />, 
+    errorElement: <ErrorPage />,
     children:[
       { path: '/',  element: <HomePage /> },
       { path: '/products',  element: <ProductsPage /> }
-    ] 
+    ]
   },
   
 ])
