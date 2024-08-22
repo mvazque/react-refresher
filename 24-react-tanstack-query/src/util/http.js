324c4c48ff759ebfa74a4a14356b811dcad680ad
Miguel Vazquez <mvazque93@gmail.com>
2024-08-22 11:10:18 -0700

    Feat(Sect-24): Implemented useQuery in FindEventSection with a dynamic query function and enabled/disabled functionality.

--- 24-react-tanstack-query/src/util/http.js
+++ 24-react-tanstack-query/src/util/http.js
@@ -1,14 +1,20 @@
-export async function fetchEvents() {
-    const response = await fetch('http://localhost:3000/events');
+export async function fetchEvents({signal, searchTerm}) {
+  let url = 'http://localhost:3000/events';
 
-    if (!response.ok) {
-      const error = new Error('An error occurred while fetching the events');
-      error.code = response.status;
-      error.info = await response.json();
-      throw error;
-    }
+  if (searchTerm) {
+    url += '?search=' + searchTerm;
+  }
 
-    const { events } = await response.json();
+  const response = await fetch(url, { signal: signal });
 
-    return events;
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the events');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { events } = await response.json();
+
+  return events;
 }
