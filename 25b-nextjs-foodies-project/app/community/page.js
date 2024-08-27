dc94530347b56b1240de154893187af3bd6086e6
Miguel Vazquez <mvazque93@gmail.com>
2024-08-27 12:23:44 -0700

    feat(Sect-25b): Created community page and made improvements to header. Implemented image slideshow.

--- 25b-nextjs-foodies-project/app/community/page.js
+++ 25b-nextjs-foodies-project/app/community/page.js
@@ -1,31 +1,40 @@
-import Link from 'next/link';
+import Image from 'next/image';
+
+import mealIcon from '@/assets/icons/meal.png';
+import communityIcon from '@/assets/icons/community.png';
+import eventsIcon from '@/assets/icons/events.png';
+import classes from './page.module.css';
 
 export default function CommunityPage() {
-    return (
-        <main>
-            <h1>Community</h1>
-            <nav>
-                <ol>
-                    <li>
-                        <Link href="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link href="/meals">Meals Page</Link>
-                    </li>
-                    <li>
-                        <Link href="/meals/share">Meals Share Page</Link>
-                    </li>
-                    <li>
-                        <Link href="/community">Community Page</Link>
-                    </li>
-                    <li>
-                        <Link href="/meals/meal-1">Dynamic Meal 1</Link>
-                    </li>
-                    <li>
-                        <Link href="/meals/meal-2">Dynamic Meal 2</Link>
-                    </li>
-                </ol>
-            </nav>
-        </main>
-    )
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          One shared passion: <span className={classes.highlight}>Food</span>
+        </h1>
+        <p>Join our community and share your favorite recipes!</p>
+      </header>
+      <main className={classes.main}>
+        <h2>Community Perks</h2>
+
+        <ul className={classes.perks}>
+          <li>
+            <Image src={mealIcon} alt="A delicious meal" />
+            <p>Share & discover recipes</p>
+          </li>
+          <li>
+            <Image src={communityIcon} alt="A crowd of people, cooking" />
+            <p>Find new friends & like-minded people</p>
+          </li>
+          <li>
+            <Image
+              src={eventsIcon}
+              alt="A crowd of people at a cooking event"
+            />
+            <p>Participate in exclusive events</p>
+          </li>
+        </ul>
+      </main>
+    </>
+  );
 }
--- 25b-nextjs-foodies-project/components/main-header/main-header.js
+++ 25b-nextjs-foodies-project/components/main-header/main-header.js
@@ -4,6 +4,8 @@ import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import { usePathname } from "next/navigation";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
     return (
@@ -17,8 +19,12 @@ export default function MainHeader() {
 
                 <nav className={classes.nav}>
                     <ul>
-                        <li><Link href="/meals">Browse Meals</Link></li>
-                        <li><Link href="/community">Foodies Community</Link></li>
+                        <li>
+                            <NavLink href="/meals">Browse Meals</NavLink>
+                        </li>
+                        <li>
+                            <NavLink href="/community">Foodies Community</NavLink>
+                        </li>
                     </ul>
                 </nav>
             </header>
