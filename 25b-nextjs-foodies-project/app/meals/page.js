6d78bf07c4d90c71fe0e9024e025816a7d3647a0
Miguel Vazquez <mvazque93@gmail.com>
2024-08-27 12:43:32 -0700

    feat(Sect-25b): Initialized work for meals page.

--- 25b-nextjs-foodies-project/app/meals/page.js
+++ 25b-nextjs-foodies-project/app/meals/page.js
@@ -1,32 +1,24 @@
-import Link from "next/link";
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
 
 export default function MealsPage() {
     return(
-        <main>
-            <h1>Meals Page</h1>
-            <nav>
-                <ol>
-                    <li>
-                        <Link href="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link href="/meals">Meals Page</Link>
-                    </li>
-                    <li>
-                        <Link href="/meals/share">Meals Share Page</Link>
-                    </li>
-                    <li>
-                        <Link href="/community">Community Page</Link>
-                    </li>
-                    <li>
-                        <Link href="/meals/meal-1">Dynamic Meal 1</Link>
-                    </li>
-                    <li>
-                        <Link href="/meals/meal-2">Dynamic Meal 2</Link>
-                    </li>
-                </ol>
-            </nav>
-            
-        </main>
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicious meals, created <span className={classes.highlight}>by you</span>
+                </h1>
+                <p>
+                    Choose your favorite recipe and cook it yourself. It is easy and fun!
+                </p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">Share Your Favorite Recipe</Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <MealsGrid meals={[]} />
+            </main>
+        </>
     )
 }
