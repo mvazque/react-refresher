42a8035247f2d88ca04944b26157cb70b48f3d8f
Miguel Vazquez <mvazque93@gmail.com>
2024-08-28 11:54:05 -0700

    feat(Sect-25b): Implemented meal detail page including a 404 not found page for it.

--- 25b-nextjs-foodies-project/app/meals/[mealSlug]/page.js
+++ 25b-nextjs-foodies-project/app/meals/[mealSlug]/page.js
@@ -1,32 +1,36 @@
-import Link from 'next/link';
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+import { notFound } from 'next/navigation';
 
 export default function MealPage({ params }) {
+    const meal = getMeal(params.mealSlug);
+
+    if(!meal) {
+        notFound();
+    }
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />');
     return (
-        <main>
-            <h1>Dynamic Meal Page</h1>
-            <p>{params.mealSlug}</p>
-            <nav>
-                <ol>
-                    <li>
-                        <Link href="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link href="/meals">Meals Page</Link>
-                    </li>
-                    <li>
-                        <Link href="/meals/share">Meals Share Page</Link>
-                    </li>
-                    <li>
-                        <Link href="/community">Community Page</Link>
-                    </li>
-                    <li>
-                        <Link href="/meals/meal-1">Dynamic Meal 1</Link>
-                    </li>
-                    <li>
-                        <Link href="/meals/meal-2">Dynamic Meal 2</Link>
-                    </li>
-                </ol>
-            </nav>
-        </main>
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image fill src={meal.image} alt={meal.title} />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p className={classes.instructions} dangerouslySetInnerHTML={{
+                    __html: meal.instructions,
+                }}></p>
+            </main>
+        </>
+        
     )
 }
