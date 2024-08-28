946769ced4e8f43dda6cd548b467835fc04ebfc0
Miguel Vazquez <mvazque93@gmail.com>
2024-08-28 11:06:07 -0700

    Feat(Sect-25b): Implemented Error and Notfound pages.

--- 25b-nextjs-foodies-project/lib/meals.js
+++ 25b-nextjs-foodies-project/lib/meals.js
@@ -4,5 +4,8 @@ const db = sql('meals.db');
 
 export async function getMeals(){
     await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    // Error used for testing.
+    // throw new Error('Loading meals failed');
     return db.prepare('SELECT * FROM meals').all();
 }
