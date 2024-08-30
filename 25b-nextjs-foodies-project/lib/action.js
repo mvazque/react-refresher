9b8be907ec2e7bc2af881d22ae68192dae1ead99
Miguel Vazquez <mvazque93@gmail.com>
2024-08-30 13:54:55 -0700

    feat(Sect-25b): Implemented error handling for form submission.

--- 25b-nextjs-foodies-project/lib/action.js
+++ 25b-nextjs-foodies-project/lib/action.js
@@ -3,16 +3,34 @@
 import { redirect } from "next/navigation";
 import { deleteMeal, saveMeal } from "./meals";
 
-export async function shareMeal(formData) {
-    const meal = {
-      title: formData.get('title'),
-      summary: formData.get('summary'),
-      instructions: formData.get('instructions'),
-      image: formData.get('image'),
-      creator: formData.get('name'),
-      creator_email: formData.get('email')
-    }
+function isInvalidText(text){
+  return !text || text.trim() === '';
+}
 
-    await saveMeal(meal);
-    redirect('/meals')
+export async function shareMeal(prevState, formData) {
+  const meal = {
+    title: formData.get('title'),
+    summary: formData.get('summary'),
+    instructions: formData.get('instructions'),
+    image: formData.get('image'),
+    creator: formData.get('name'),
+    creator_email: formData.get('email')
+  }
+
+  if(
+    isInvalidText(meal.title) || 
+    isInvalidText(meal.summary) || 
+    isInvalidText(meal.instructions) || 
+    isInvalidText(meal.creator) || 
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes('@') ||
+    !meal.image || meal.image.size === 0
+  ) {
+    return {
+      message: 'Invalid input.'
+    };
+  }
+
+  await saveMeal(meal);
+  redirect('/meals')
 }
