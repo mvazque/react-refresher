722e5ece7a5fcd27fd5ab976f51edf7d5f87a09f
Miguel Vazquez <mvazque93@gmail.com>
2024-09-01 18:45:19 -0700

    feat(Sect-25c): Implemented nest routes in pages configuration. Implemented dynamic route and links.

---
+++ 25c-nextjs-page-router/pages/news/[newsId].js
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router"
+
+function DetailPage() {
+    const router = useRouter();
+
+    const newsId = router.query.newsId;
+    return (
+        <>
+            <h1>The Detail Page</h1>
+            <p>{newsId}</p>
+        </>
+        
+    )
+}
+
+export default DetailPage
