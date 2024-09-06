66e8f5bc1f366f56f7ee2bdf339d0021b1ff218a
Miguel Vazquez <mvazque93@gmail.com>
2024-09-06 14:14:56 -0700

    feat(Sect-25d): Implemented static rendering.

--- 25d-advanced-nextjs-pages-router/pages/[meetupId]/index.js
+++ 25d-advanced-nextjs-pages-router/pages/[meetupId]/index.js
@@ -11,4 +11,41 @@ function MeetupDetails() {
     )
 }
 
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1',
+                }
+            },
+            {
+                params: {
+                    meetupId: 'm2',
+                }
+            }
+        ]
+    }
+}
+
+export async function getStaticProps(context) {
+
+    const meetupId = context.params.meetupId;
+
+    console.log(meetupId);
+
+    return {
+        props: {
+            meetupData: {
+                image: "https://wallpapers.com/images/high/st-paul-dome-sky-cloudy-b3vk37rdgkh5pgyu.webp",
+                id: meetupId,
+                title: 'First Meeting',
+                address: 'Some address 5, 12345 Some City',
+                description: 'The meetup description'
+            }
+        }
+    }
+}
+
 export default MeetupDetails;
