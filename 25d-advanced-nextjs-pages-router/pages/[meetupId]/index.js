13b48509316f6f5d7141899aa09194758af051d2
Miguel Vazquez <mvazque93@gmail.com>
2024-09-06 12:19:48 -0700

    feat(Sect-25b): Implemented MeetupDetail page and component along with styling.

---
+++ 25d-advanced-nextjs-pages-router/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+    return (
+        <MeetupDetail 
+            image="https://wallpapers.com/images/high/st-paul-dome-sky-cloudy-b3vk37rdgkh5pgyu.webp"
+            title="A First Meetup"
+            description="The meetup description"
+            address="Some address 5, 12345 Some City"
+        />
+    )
+}
+
+export default MeetupDetails;
