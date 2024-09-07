77585a19a3ced90132c6e103362e5e3505ce6166
Miguel Vazquez <mvazque93@gmail.com>
2024-09-06 17:08:03 -0700

    feat(Sect-25d): Implemented GET and POST requests in our Server Components.

--- 25d-advanced-nextjs-pages-router/pages/index.js
+++ 25d-advanced-nextjs-pages-router/pages/index.js
@@ -1,3 +1,5 @@
+import { MongoClient } from 'mongodb';
+
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_MEETUPS = [
@@ -35,9 +37,22 @@ function HomePage(props) {
 // }
 
 export async function getStaticProps() {
+
+    const client = await MongoClient.connect('mongodb+srv://{{username}}:{{password}}@udemy-test.ehar5.mongodb.net/meetups?retryWrites=true&w=majority&appName=Udemy-Test')
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const meetups = await meetupsCollection.find().toArray();
+
     return {
         props: {
-            meetups: DUMMY_MEETUPS
+            meetups: meetups.map((meetup) => ({
+                title: meetup.title,
+                address: meetup.address,
+                image: meetup.image,
+                id: meetup._id.toString()
+            }))
         },
         revalidate: 10
     }
