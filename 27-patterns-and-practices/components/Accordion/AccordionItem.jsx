e4c41f5723d1a9d4987cc998059b188ec1f39d2f
Miguel Vazquez <mvazque93@gmail.com>
2024-09-13 11:00:04 -0700

    feat(Sect-27): Implemented Accordion and AccordionItem. Created context to be shared from Accordion to AccordionItem.

---
+++ 27-patterns-and-practices/components/Accordion/AccordionItem.jsx
@@ -0,0 +1,22 @@
+import { useAccordionContext } from "./Accordion"
+
+export default function AccordionItem({ id, title, children, className}) {
+    const { openItemId, openItem, closeItem } = useAccordionContext();
+
+    const isOpen = openItemId === id;
+
+    function handleClick() {
+        if(isOpen) {
+            closeItem();
+        } else {
+            openItem(id);
+        }
+    }
+    
+    return (
+        <li className={className}>
+            <h3 onClick={handleClick}>{title}</h3>
+            <div className={isOpen ? 'accordion-item-content open' : 'accordion-item-content'}>{children}</div>
+        </li>
+    )
+}
