e123ba66b353ab71e701a607f87211d0bd594ba9
Miguel Vazquez <mvazque93@gmail.com>
2024-09-13 12:59:28 -0700

    Feat(Sect-27): Implemented prop function for dynamic key creation.

--- 27-patterns-and-practices/components/SearchableList/SearchableList.jsx
+++ 27-patterns-and-practices/components/SearchableList/SearchableList.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function SearchableList({items, children}) {
+export default function SearchableList({items, itemKeyFn, children}) {
     const [searchTerm, setSearchTerm] = useState('');
 
     const searchResults = items.filter((item) => JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase()));
@@ -12,8 +12,8 @@ export default function SearchableList({items, children}) {
     return <div className="searchable-list">
         <input type="search" placeholder="Search" onChange={handleChange} />
         <ul>
-            {searchResults.map((item, index) => (
-                <li key={index}>{children(item)}</li>
+            {searchResults.map((item) => (
+                <li key={itemKeyFn(item)}>{children(item)}</li>
             ))}
         </ul>
     </div>
