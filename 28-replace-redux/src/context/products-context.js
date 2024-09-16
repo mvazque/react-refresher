1e1764e24cd654f350e88c2e2dc325cace0ae281
Miguel Vazquez <mvazque93@gmail.com>
2024-09-16 09:54:34 -0700

    Feat(Sect-28): Implemented toggleFav.

--- 28-replace-redux/src/context/products-context.js
+++ 28-replace-redux/src/context/products-context.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 
-export const ProductsContext = React.createContext({products: []})
+export const ProductsContext = React.createContext({
+    products: [],
+    toggleFav: (id) => {}
+})
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default props => {
     const [productsList, setProductsList] = useState([
         {
@@ -29,8 +33,24 @@ export default props => {
           isFavorite: false
         }
       ]);
+
+
+    const toggleFavorite = productId => {
+        setProductsList(currentProdList => {
+            const prodIndex = currentProdList.findIndex(
+                p => p.id === productId
+            );
+            const newFavStatus = !currentProdList[prodIndex].isFavorite;
+            const updatedProducts = [...currentProdList];
+            updatedProducts[prodIndex] = {
+                ...currentProdList[prodIndex],
+                isFavorite: newFavStatus
+            };
+            return updatedProducts;
+        })
+    }
     return(
-        <ProductsContext.Provider value={{products: productsList}}>
+        <ProductsContext.Provider value={{products: productsList, toggleFav: toggleFavorite}}>
             {props.children}
         </ProductsContext.Provider>
     );
